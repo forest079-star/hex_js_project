5770372999a6a15e89495f391281437a8a5025cb
forest079 <bluegx08@gmail.com>
2020-07-13 10:50:02 +0800

    add week2 fold

---
+++ week2/app/assets/js/all.js
@@ -0,0 +1,28 @@
+$(document).ready(() => {
+  console.log('Hello Bootstrap4');
+
+
+
+  var obj = {
+    data:{
+      uuid:'',
+      category:'',
+      products:[],
+    },
+
+    getData() {
+
+    },
+
+    sortData(){
+
+    },
+
+    render(){
+
+
+      //隱藏loading...
+      document.querySelector('.loading-background').style="display:none !important";
+    },
+  }
+});
